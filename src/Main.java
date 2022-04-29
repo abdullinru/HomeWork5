22e610c62e02bc26d89da4cc986d2ec3ac3e52b7
abdullinru <abdullin17021989@gmail.com>
2022-04-30 00:08:59 +0600

    Четвертое задание

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        task3();
+        task4();
     }
-    public static void task3(){
+    public static void task4(){
         int[] masInt = new int[3];
         masInt[0] = 1;
         masInt[1] = 2;
         masInt[2] = 3;
-        double[] masDouble = {1.57, 7.654, 9.986};
-        char[] masChar = {'a','b','c'};
-        for(int i = masInt.length-1; i >=0; i--){
-            System.out.print(masInt[i]);
-            if (i==0)
-                continue;
-            System.out.print(", ");
+        for(int i=0; i < masInt.length;i++){
+            if (masInt[i]%2==1){
+                masInt[i]++;
+            }
+            System.out.print(masInt[i]+ " ");
         }
-        System.out.println();
-        for(int i = masDouble.length-1; i >=0; i--){
-            System.out.print(masDouble[i]);
-            if (i==0)
-                continue;
-            System.out.print(", ");
-        }
-        System.out.println();
-        for(int i = masChar.length-1; i >=0; i--){
-            System.out.print(masChar[i]);
-            if (i==0)
-                continue;
-            System.out.print(", ");
-        }
-        System.out.println();
-
     }
 }
