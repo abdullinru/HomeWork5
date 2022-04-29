e82c49627aa8aa19d0065c4aba4ac35ac2c77ad4
abdullinru <abdullin17021989@gmail.com>
2022-04-29 23:46:16 +0600

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1(){
+        int[] masInt = new int[3];
+        masInt[0] = 1;
+        masInt[1] = 2;
+        masInt[2] = 3;
+
+        double[] masDouble = {1.57, 7.654, 9.986};
+
+        char[] masChar = {'a','b','c'};
     }
 }
